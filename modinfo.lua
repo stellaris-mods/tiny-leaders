bdfa45735d08889ac3ef4e1d37f2d3fdffd98c6d
Folk <vhaarr@gmail.com>
2018-02-11 19:37:16 +0100

    Initial commit.

---
+++ modinfo.lua
@@ -0,0 +1,12 @@
+-- modinfo read by stlrel from http://github.com/stellaris-mods/scripts
+return {
+	path = "folk_tinyleaders",
+	name = "Tiny Leaders",
+	tags = { "Graphics", "Utilities" },
+	picture = "thumb.png",
+	supported_version = "1.9.*",
+	originalUploader = "folk",
+	--remote_file_id = 0,
+	readme = "readme.md",
+	steambb = "steam.bbcode",
+}
